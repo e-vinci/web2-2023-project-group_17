bf1504454b5106a04f4a7469fc8666854ff74b8e
Ysaline-Degols-Vinci <128386131+Ysaline-Degols-Vinci@users.noreply.github.com>
2023-11-13 21:56:40 +0100

    adding logo on the homepage

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,4 +1,5 @@
 import backgroundImg from '../../img/background_clouds.png';
+import logo from '../../img/logo_nekocafe.png';
 
 
 const HomePage = () => {
@@ -7,6 +8,9 @@ const HomePage = () => {
   <div style="height: 100vh; display: flex; align-items: center; justify-content: center; background-image: url('${backgroundImg}'); background-size: cover; background-repeat: no-repeat; background-position: center;">
     <div style="height:100%; width:100%;">
   </div>
+
+  <img src="${logo}" alt="Your Image" class="center-logo">
+
   `
   main.innerHTML = homePage;
 };
