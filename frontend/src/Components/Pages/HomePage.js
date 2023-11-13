a16b09acfbee45ffc843c07cb7e099a9d51f5fa5
Naturel1 <moi@arnaudhubert.be>
2023-11-13 15:45:24 +0100

    add background (repeat to solv)

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,14 @@
+import backgroundImg from '../../img/background_clouds.png';
+
+
 const HomePage = () => {
   const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+  const homePage = `
+  <div style="height: 100vh; display: flex; align-items: center; justify-content: center; background-image: url('${backgroundImg}'); background-size: contain; background-repeat: no-repeat !important; background-position: center;">
+    <div style="background-image: url('${backgroundImg}'); height:100%; width:100%;">
+  </div>
+  `
+  main.innerHTML = homePage;
 };
 
 export default HomePage;
