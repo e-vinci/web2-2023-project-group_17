e0b2fcc725ac675b2337b552efe78ba2d7fa869b
Ysaline-Degols-Vinci <128386131+Ysaline-Degols-Vinci@users.noreply.github.com>
2023-11-17 09:24:14 +0100

    adding background to new pages

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -1,7 +1,12 @@
+import backgroundImg from '../../img/background_clouds.png';
+
+
 const RegisterPage = () => {
     const main = document.querySelector('main');
     const registerPage = `
-    
+    <div style="height: 100vh; display: flex; align-items: center; justify-content: center; background-image: url('${backgroundImg}'); background-size: cover; background-repeat: no-repeat; background-position: center;">
+    <div style="height:100%; width:100%;">
+    </div>
   
   
   
--- frontend/src/Components/Pages/loginPage.js
+++ frontend/src/Components/Pages/loginPage.js
@@ -1,7 +1,12 @@
+import backgroundImg from '../../img/background_clouds.png';
+
+
 const LoginPage = () => {
   const main = document.querySelector('main');
   const loginPage = `
-  
+    <div style="height: 100vh; display: flex; align-items: center; justify-content: center; background-image: url('${backgroundImg}'); background-size: cover; background-repeat: no-repeat; background-position: center;">
+    <div style="height:100%; width:100%;">
+    </div>
 
 
 
