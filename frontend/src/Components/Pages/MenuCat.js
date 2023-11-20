483816cda23477926d0f786fd4472254e2fd4dfb
Orphelia Agbemadon <orphelia.agbemadon@student.vinci.be>
2023-11-20 22:05:55 +0100

    Adding quit button to MenuCat page

--- frontend/src/Components/Pages/MenuCat.js
+++ frontend/src/Components/Pages/MenuCat.js
@@ -2,6 +2,7 @@ import backgroundImg from '../../img/background_clouds.png';
 import buddyImg from '../../img/cat_buddy.png';
 import whiskersImg from '../../img/cat_whiskers.png';
 import miaouImg from '../../img/cat_miaou.png';
+import quitImg from '../../img/close_button.png';
 
 import Navigate from '../Router/Navigate';
 
@@ -9,17 +10,17 @@ const cats = [
   {
     name: 'Buddy',
     bonus: 10,
-    photo: buddyImg
+    photo: buddyImg,
   },
   {
     name: 'Whiskers',
     bonus: 5,
-    photo: whiskersImg
+    photo: whiskersImg,
   },
   {
     name: 'Miaou',
     bonus: 20,
-    photo: miaouImg
+    photo: miaouImg,
   },
 ];
 
@@ -40,15 +41,20 @@ const MenuCat = () => {
 
   const menuCat = `
         <div style="height: 100vh; display: flex; align-items: center; justify-content: center; background-image: url('${backgroundImg}'); background-size: cover; background-repeat: no-repeat; background-position: center;">
-        <div style="height:100%; width:100%;">
-        <div style="display: flex; justify-content: center; margin-bottom: 20px;">
-        <button id="coffee-button" style="padding: 10px; font-size: 16px;">Coffee Button</button>
-        <button id="cat-button" style="padding: 10px; font-size: 16px;">Cat Button</button>
-        </div>
-        <div style="display: flex; justify-content: center;">
-        ${catHTML}
-        </div>
-      </div>`;
+          
+          <div style="height:100%; width:100%;">
+            <div style="position: absolute; top: 20%; left: 0; transform: translateY(-50%);">
+              <img src="${quitImg}" alt="Bouton quitter" id="quit-button" style="width: 50px; height: 50px;">
+            </div>
+            <div style="display: flex; justify-content: center; margin-bottom: 20px;">
+              <button id="coffee-button" style="padding: 10px; font-size: 16px;">Coffee Button</button>
+              <button id="cat-button" style="padding: 10px; font-size: 16px;">Cat Button</button>
+            </div>
+            <div style="display: flex; justify-content: center;"> 
+              ${catHTML}
+            </div>
+          </div>  
+        </div>`;
   main.innerHTML = menuCat;
 
   const coffeeButton = document.querySelector('#coffee-button');
@@ -62,6 +68,9 @@ const MenuCat = () => {
   const catButton = document.querySelector('#cat-button');
   catButton?.addEventListener('click', redirectToMenuCat);
   catButton.style.backgroundColor = 'pink';
+  
+  const quitButton = document.querySelector('#quit-button');
+  quitButton?.addEventListener('click',redirectToMenu);
 };
 
 function redirectToMenuCat() {
