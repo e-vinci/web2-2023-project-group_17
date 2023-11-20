67e24183d7bb52a2fc08128cebb81b2f37b4b0c2
sinem-bayrak-vinci <sinem.bayrak@student.vinci.be>
2023-11-20 15:30:34 +0100

    create MenuPage and buttons

---
+++ frontend/src/Components/Pages/MenuPage.js
@@ -0,0 +1,38 @@
+import backgroundImg from '../../img/background_clouds.png';
+
+import Navigate from '../Router/Navigate';
+
+
+const MenuPage = () => {
+  const main = document.querySelector('main');
+  const menuPage = `
+    <div style="height: 100vh; display: flex; align-items: center; justify-content: center; background-image: url('${backgroundImg}'); background-size: cover; background-repeat: no-repeat; background-position: center;">
+      <div style="height:100%; width:100%;">
+      <button id="coffee-button" style="padding: 10px; font-size: 16px;">Coffee Button</button>
+      <button id="cat-button" style="padding: 10px; font-size: 16px;">Cat Button</button>
+    </div>`;
+  main.innerHTML = menuPage;
+
+  const coffeeButton = document.querySelector('#coffee-button');
+  coffeeButton?.addEventListener('click', redirectToMenu);
+  coffeeButton?.addEventListener('mouseover', () => {
+    coffeeButton.style.backgroundColor = 'pink';
+  });
+  coffeeButton?.addEventListener('mouseout', () => {
+    coffeeButton.style.backgroundColor = 'white';
+  });
+  const catButton = document.querySelector('#cat-button');
+  catButton?.addEventListener('click', redirectToMenu);
+  catButton?.addEventListener('mouseover', () => {
+    catButton.style.backgroundColor = 'pink';
+  });
+  catButton?.addEventListener('mouseout', () => {
+    catButton.style.backgroundColor = 'white';
+  });
+};
+
+function redirectToMenu() {
+    Navigate('/menu');
+  }
+  
+  export default MenuPage;
