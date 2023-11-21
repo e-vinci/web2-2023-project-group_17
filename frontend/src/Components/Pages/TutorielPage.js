330dc78759f325feb708953dc9d46bf4134c53c2
Orphelia Agbemadon <orphelia.agbemadon@student.vinci.be>
2023-11-21 09:45:37 +0100

    Adding acces to tutorial page

--- frontend/src/Components/Pages/TutorielPage.js
+++ frontend/src/Components/Pages/TutorielPage.js
@@ -3,11 +3,13 @@ import backgroundImg from '../../img/background_clouds.png';
 
 const TutorielPage = () => {
     const main = document.querySelector('main');
+    document.title = 'Tutoriel';
     const tutorielPage = `
     <div style="height: 100vh; display: flex; align-items: center; justify-content: center; background-image: url('${backgroundImg}'); background-size: cover; background-repeat: no-repeat; background-position: center;">
     <div style="height:100%; width:100%;">
     </div>
-  
+    <div>
+      <h2>
   
   
     `
