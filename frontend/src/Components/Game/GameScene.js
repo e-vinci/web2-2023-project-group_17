ef7bbb863003ee2c99e7b088eb1e038b36a700c3
Ysaline-Degols-Vinci <128386131+Ysaline-Degols-Vinci@users.noreply.github.com>
2023-11-22 22:28:44 +0100

    add character + make it move

--- frontend/src/Components/Game/GameScene.js
+++ frontend/src/Components/Game/GameScene.js
@@ -14,6 +14,8 @@ const MOVE_LEFT_KEY = 'walkLeft';
 class GameScene extends Phaser.Scene {
   constructor() {
     super('game-scene');
+    this.player = undefined;
+    this.cursors = undefined;
   }
 
   
@@ -21,19 +23,16 @@ class GameScene extends Phaser.Scene {
     this.load.image('map', bgScene);
     this.load.image('background', backgroundImg);
     this.load.spritesheet(IDLE_KEY, idleSprite, {
-      frameWidth: 100,
-      frameHeight: 16,
-      // put randomly gotta fix this
+      frameWidth: 24,
+      frameHeight: 22,
     });
     this.load.spritesheet(MOVE_RIGHT_KEY, walkRightSprite, {
-      frameWidth: 100,
-      frameHeight: 16,
-      // put randomly gotta fix this
+      frameWidth: 24,
+      frameHeight: 23,
     });
     this.load.spritesheet(MOVE_LEFT_KEY, walkLeftSprite, {
-      frameWidth: 100,
-      frameHeight: 16,
-      // put randomly gotta fix this
+      frameWidth: 24,
+      frameHeight: 21.5,
     });
   }
 
@@ -46,17 +45,66 @@ class GameScene extends Phaser.Scene {
 
     const map = this.add.image(0, 0, 'map').setOrigin(-0.45, 0);
     map.setScale(0.39);
-
-    
-    
-   
-  
-    
+    this.player = this.createPlayer();
+    this.cursors = this.input.keyboard.createCursorKeys();
+      
   }
 
-  // eslint-disable-next-line class-methods-use-this
+ 
   update() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+      this.player.anims.play('left', true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+     
+      this.player.anims.play('right', true);
+    } else if (this.cursors.down.isDown) {
+      this.player.setVelocityY(160);
+      this.player.anims.play('right', true);
+    } 
+    else if (this.cursors.up.isDown) {
+      this.player.setVelocityY(-160);
+      this.player.anims.play('right', true);
+    } 
+    else {
+      this.player.setVelocityX(0);
+      this.player.setVelocityY(0);
+      this.player.anims.play('idleAnim', true);
+    }
+    
   }
+
+createPlayer(){
+  const player = this.physics.add.sprite(750, 450, IDLE_KEY);
+  player.setScale(2.8);
+  player.setCollideWorldBounds(true);
+  player.body.setAllowGravity(false);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers(MOVE_LEFT_KEY, { start: 0, end: 7 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers(MOVE_RIGHT_KEY, { start: 0, end: 7 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: 'idleAnim',
+    frames: this.anims.generateFrameNumbers(IDLE_KEY, { start: 0, end: 1}),
+    frameRate: 3,
+    repeat: -1,
+  });
+
+  return player;
+}
+  
 }
 
 export default GameScene;
