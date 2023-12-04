a7a8be97a7c81b57b459e2e76e51cd21b98b0728
Orphelia Agbemadon <orphelia.agbemadon@student.vinci.be>
2023-12-04 15:30:47 +0100

    Changing tutorial text

--- frontend/src/Components/Pages/TutorielPage.js
+++ frontend/src/Components/Pages/TutorielPage.js
@@ -8,23 +8,22 @@ import Navigate from '../Router/Navigate';
 
 const TutorielPage = () => {
   const main = document.querySelector('main');
-  document.title = 'Tutoriel';
+  document.title = 'Neko café';
 
   const tutorielPage = `
     <div style="height: 100vh; display: flex; align-items: center; justify-content: center; background-image: url('${backgroundImg}'); background-size: cover; background-repeat: no-repeat; background-position: center;">
-      <div style="position: absolute; width: 20%; top: 0; left: 0;">
+      <div style="position: absolute; width: 15%; top: 0; left: 0;">
         <img src="${homepageIcone}" alt="homepage-icon" id="homepage-button">
       </div>  
         <div style="position: absolute; bottom: 0; left: 10%;">
           <img src="${characterImg}" alt="character-image" style="width: 400px;">
         </div>
-        <div  style="position: absolute; width: 850px; z-index: 1; top: 50%; left: 70%; transform: translate(-50%, -50%);">
+        <div  style="position: absolute; width: 850px; heigth: 100px; z-index: 1; top: 50%; left: 70%; transform: translate(-50%, -50%);">
           <img src="${bubbleImg}" alt="bubble image" style="width: 100%;">
         </div>
-        <div style="font-family: 'Press Start 2P', cursive; position: absolute; z-index: 2; padding: 20px;  top: 50%; left: 60%; transform: translate(-50%, -50%); text-align: left;">
+        <div style="font-family: 'Press Start 2P', cursive; position: absolute; z-index: 2; padding: 20px;  top: 50%; left: 65%; transform: translate(-50%, -50%); text-align: left;">
           
           <h6 style="font-size: 1em; text-align: center;"> Bienvenue au Neko Café! </h6> 
-            <br>
               <p style="font-size: 0.7em"> 
                 <b> But du jeu : </b> Gagner de l'argent en vendant des cafés et en caressant des chats.
               </p>
@@ -36,9 +35,6 @@ const TutorielPage = () => {
           </p>
           <p style="font-size: 0.7em">
             <b> Tableau de classement: </b> Votre score est calculé sur base de l'argent gagné.
-          </p>
-          <p style="font-size: 0.7em; text-align: center;">
-            Bonne partie !
           </p>
       </div>
     </div>
