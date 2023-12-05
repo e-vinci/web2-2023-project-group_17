70508a75e83bc3b2833df8464ae622980df95071
Orphelia Agbemadon <orphelia.agbemadon@student.vinci.be>
2023-12-05 18:23:46 +0100

    Changing speech bubble

--- frontend/src/Components/Pages/TutorielPage.js
+++ frontend/src/Components/Pages/TutorielPage.js
@@ -1,6 +1,6 @@
 import backgroundImg from '../../img/background_clouds.png';
 import characterImg from '../../img/tutoriel_character.png';
-import bubbleImg from '../../img/speech_bubble.png';
+import bubbleImg from '../../img/bubble_speech.png';
 import homepageIcone from '../../img/accueil_button.png';
 import homepageIconeHover from '../../img/hovered_accueil.png';
 
@@ -18,17 +18,19 @@ const TutorielPage = () => {
         <div style="position: absolute; bottom: 0; left: 10%;">
           <img src="${characterImg}" alt="character-image" style="width: 400px;">
         </div>
-        <div  style="position: absolute; width: 850px; heigth: 100px; z-index: 1; top: 50%; left: 70%; transform: translate(-50%, -50%);">
+        <div  style="position: absolute; width: 980px; heigth: 100px; z-index: 1; top: 50%; left: 70%; transform: translate(-50%, -50%);">
           <img src="${bubbleImg}" alt="bubble image" style="width: 100%;">
         </div>
-        <div style="font-family: 'Press Start 2P', cursive; position: absolute; z-index: 2; padding: 20px;  top: 50%; left: 65%; transform: translate(-50%, -50%); text-align: left;">
-          
+        <div style="font-family: 'Press Start 2P', cursive; position: absolute; z-index: 2; padding: 20px;  top: 45%; left: 65%; transform: translate(-50%, -50%); text-align: left;">
           <h6 style="font-size: 1em; text-align: center;"> Bienvenue au Neko Café! </h6> 
               <p style="font-size: 0.7em"> 
-                <b> But du jeu : </b> Gagner de l'argent en vendant des cafés et en caressant des chats.
+                <b> But du jeu : </b> Gagner de l'argent en vendant des cafés et en cliquant sur des chats.
               </p>
           <p style="font-size: 0.7em">
-            <b> Comment jouer ? </b> Inscrivez-vous via le bouton "nouveau compte" ou connectez-vous si vous avez déjà un compte.
+            <b> Comment accéder au jeu ? </b> Inscrivez-vous via le bouton "nouveau compte" ou connectez-vous si vous avez déjà un compte.                     
+          </p>
+          <p style="font-size: 0.7em">
+          <b> Comment jouer au jeu ? </b> Déplacez votre personnage avec les flèches de votre clavier.Cliquez sur les chats ou bien laissez les clients entrer et sortir pour gagner des CatCoins.
           </p>
           <p style="font-size: 0.7em">
            <b> Améliorations des boissons et des chats: </b> Utilisez vos CatCoins dans l'onglet "Menu".Cela attirera plus de clients;)
