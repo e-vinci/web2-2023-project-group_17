3b4c0dda2b837210cbc48a1fd45716e0165edb25
SashaVanRoss <118728924+SashaVanRoss@users.noreply.github.com>
2023-12-10 19:25:54 +0100

    refactor user.js in api impl api call in frontend

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,78 +4,47 @@ const {authorize} = require('../utils/auths');
 const {parse, serialize} = require("../utils/json");
 
 const jsonDbPath = path.join(__dirname, '/../data/highScores.json');
-
 const router = express.Router();
 
-/**
- * set the score of the specified user to the new score
- * @param authorize this middleware verify if the request is authorized before allowing it to proceed
- */
-// eslint-disable-next-line consistent-return
 router.post('/set', authorize, (req, res) => {
-    const {username} = req.user;
-    if ( req?.body?.score === undefined) return res.status(400).json({error: 'Score missing !'}); // Bad Request
+    const {username, body: {score}} = req.user;
 
-    if (!username) return res.status(400).json({error: 'Username missing !'}); // Bad Request
+    if (!username) return res.status(400).json({error: 'Username missing !'});
+    if (!score) return res.status(400).json({error: 'Score missing !'});
 
     const highScores = parse(jsonDbPath);
+    const userScore = highScores.find(highScore => highScore.username === username);
 
-    highScores.forEach((highScore) => {
-        if (highScore.username === username) {
-            // eslint-disable-next-line no-param-reassign
-            highScore.score = req.body.score;
-        }
-    });
+    if (userScore && userScore.score < score) userScore.score = score;
 
     serialize(jsonDbPath, highScores);
-    return res.sendStatus(200); // OK
+    return res.sendStatus(200);
 });
-/**
- * get the Score of the specified user
- * @param authorize this middleware verify if the request is authorized before allowing it to proceed
- * @returns the score at wich he user is or 0 if the user has never played
- */
-// eslint-disable-next-line consistent-return
+
 router.post('/get', authorize, (req, res) => {
     const {username} = req.user;
 
-    if (!username) return res.status(400).json({error: 'Username missing !'}); // Bad Request
+    if (!username) return res.status(400).json({error: 'Username missing !'});
 
     const highScores = parse(jsonDbPath);
+    let userScore = highScores.find(highScore => highScore.username === username);
 
-    let found;
-    // eslint-disable-next-line consistent-return
-    highScores.forEach((highScore) => {
-        if (highScore.username === username) {
-            found = highScore;
-        }
-    });
-
-    if (found) {
-        return res.status(200).json(found); // OK
+    if (!userScore) {
+        userScore = {username, score: 0};
+        highScores.push(userScore);
+        serialize(jsonDbPath, highScores);
     }
-    const newhighScore = {
-        username,
-        score: 0,
-    };
-    highScores.push(newhighScore);
-    serialize(jsonDbPath, highScores);
-    return res.status(200).json(newhighScore); // OK
-});
 
-/**
- * @returns the leaderboard sorted by score or an error if there is no score registered in the leaderboard
- */
+    return res.status(200).json(userScore);
+});
 
 router.get('/scores', (req, res) => {
     const scores = parse(jsonDbPath);
 
-    if (scores) {
-        scores.sort((a, b) => b.score - a.score);
-        return res.status(200).json(scores); // OK
-    }
-    return res.status(404).json({error: 'No leaderboard found'}); // Not Found
+    if (!scores) return res.status(404).json({error: 'No leaderboard found'});
 
+    scores.sort((a, b) => b.score - a.score);
+    return res.status(200).json(scores);
 });
 
 module.exports = router;
