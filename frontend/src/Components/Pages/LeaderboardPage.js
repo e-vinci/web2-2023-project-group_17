f484ed9772cc7359bc248c9446bd03a64f4b73e5
Ysaline-Degols-Vinci <128386131+Ysaline-Degols-Vinci@users.noreply.github.com>
2023-12-12 23:37:52 +0100

    adding style + changing redirect

--- frontend/src/Components/Pages/LeaderboardPage.js
+++ frontend/src/Components/Pages/LeaderboardPage.js
@@ -1,4 +1,7 @@
 import Navigate from "../Router/Navigate";
+import backgroundImg from '../../img/background_clouds.png';
+import homepageIcone from '../../img/accueil_button.png';
+import homepageIconeHover from '../../img/hovered_accueil.png';
 
 const buildTab = (scores) => {
     const headHtml =`<tr>
@@ -33,7 +36,21 @@ const LeaderboardPage = async () => {
     const main = document.querySelector("main");
     main.innerHTML = ``
 
+    main.style.backgroundImage = `url('${backgroundImg}')`;
+    main.style.backgroundSize = 'cover'; 
+
+    
+  const section = document.createElement('section');
+  section.className = 'section';
+  section.style.position = 'absolute';
+  section.style.top = '20%';
+  
     const title = document.createElement("h1")
+    title.style.marginLeft = "38%";
+    title.style.marginTop = "3%";
+    section.appendChild(title);
+
+
     title.innerText = "Meilleurs scores"
     main.appendChild(title)
 
@@ -47,14 +64,31 @@ const LeaderboardPage = async () => {
 
     main.appendChild(buildTab(scores));
 
-    const btnGame = document.createElement("button")
-    btnGame.innerText = "Retournez au jeu"
+    // const btnGame = document.createElement("button")
+    // btnGame.innerText = "Retournez au jeu"
+
+    // main.appendChild(btnGame)
+    const btnGame = document.createElement("img");
+
+    btnGame.src = homepageIcone;
+    btnGame.style.width = '240px';  
+    btnGame.style.height = '180px'; 
+    btnGame.style.marginTop = "24%";
 
     main.appendChild(btnGame)
 
+    btnGame.addEventListener("mouseover", () => {
+        btnGame.src = homepageIconeHover;
+    });
+    
+    btnGame.addEventListener("mouseout", () => {
+        btnGame.src = homepageIcone;
+    });
+    
+
     btnGame.addEventListener("click", (event => {
         event.preventDefault()
-        Navigate('/game')
+        Navigate('/')
     }))
 };
   
