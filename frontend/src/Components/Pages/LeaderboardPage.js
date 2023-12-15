7487c4f63d4fc1edd44097c1a7865b93152b7f07
SashaVanRoss <118728924+SashaVanRoss@users.noreply.github.com>
2023-12-15 20:30:46 +0100

    LeaderboardPage.js styled

--- frontend/src/Components/Pages/LeaderboardPage.js
+++ frontend/src/Components/Pages/LeaderboardPage.js
@@ -1,58 +1,55 @@
 import Navigate from "../Router/Navigate";
 import backgroundImg from '../../img/background_clouds.png';
-import homepageIcone from '../../img/accueil_button.png';
-import homepageIconeHover from '../../img/hovered_accueil.png';
-
-const buildTab = (scores) => {
-    const headHtml =`<tr>
-        <th>Pseudo</th>
-        <th>Score</th>
-    </tr>`
-
-    const tab = document.createElement('table')
-    tab.style = "border: 3px solid"
-
-    const tabThead = tab.createTHead()
-    tabThead.innerHTML = headHtml
-
-    const tabBody = tab.createTBody()
-    scores.forEach((player) => {
-        const row = document.createElement("tr")
-        const cellUser = document.createElement("td")
-        cellUser.innerText = `${player.username}`
-        row.appendChild(cellUser)
-
-        const cellScore = document.createElement("td")
-        cellScore.innerText = `${player.score}`
-        row.appendChild(cellScore)
-
-        tabBody.appendChild(row)
-    })
-
-    return tab
-}
+import homepageIcon from '../../img/accueil_button.png';
+import homepageIconHover from '../../img/hovered_accueil.png';
 
 const LeaderboardPage = async () => {
     const main = document.querySelector("main");
-    main.innerHTML = ``
-
+    main.innerHTML = '';
     main.style.backgroundImage = `url('${backgroundImg}')`;
-    main.style.backgroundSize = 'cover'; 
-
-    
-  const section = document.createElement('section');
-  section.className = 'section';
-  section.style.position = 'absolute';
-  section.style.top = '20%';
-  
-    const title = document.createElement("h1")
-    title.style.marginLeft = "38%";
-    title.style.marginTop = "3%";
-    section.appendChild(title);
+    main.style.backgroundSize = 'cover';
+    main.style.backgroundRepeat = 'no-repeat';
+    main.style.backgroundPosition = 'center';
+    main.style.height = '100vh';
+    main.style.width = '100vw';
+    main.style.overflowY = 'scroll';
 
 
-    title.innerText = "Meilleurs scores"
-    main.appendChild(title)
+
+    const btnGame = document.createElement("img");
+    btnGame.src = homepageIcon;
+    btnGame.style.width = '240px';
+    btnGame.style.height = '180px';
+    btnGame.style.cursor = 'pointer';
+    btnGame.style.position = 'fixed';
+    btnGame.style.top = '20px';
+    btnGame.style.left = '20px';
+
+
+
+    main.appendChild(btnGame);
+
+    btnGame.addEventListener("mouseover", (() => {
+        btnGame.src = homepageIconHover
+    }));
+    btnGame.addEventListener("mouseout", (() => {
+        btnGame.src = homepageIcon
+    }));
+    btnGame.addEventListener("click", (event => {
+        event.preventDefault();
+        Navigate('/');
+    }));
+
+    const title = document.createElement("h1");
+    title.innerText = "Meilleurs scores";
+    title.style.textAlign = 'center';
+    title.style.fontSize = '3rem';
+    title.style.color = 'white';
+    title.style.textShadow = '2px 2px 2px black';
+    title.style.marginBottom = '50px';
+    title.style.paddingTop = '30px';
+
+    main.appendChild(title);
 
     const result = await fetch(`${process.env.API_BASE_URL}/users/scores`, {
         method: 'GET',
@@ -62,35 +59,30 @@ const LeaderboardPage = async () => {
     });
     const scores = await result.json();
 
-    main.appendChild(buildTab(scores));
+    const table = document.createElement('table');
+    const header = document.createElement('tr');
+    header.innerHTML = '<th style="border-right: rgba(255, 255, 255, 0.5) dashed 1px; padding-bottom: 20px; padding-top: 20px; background-color: rgba(0, 0, 0, 0.1) ">Utilisateur</th>' +
+        '<th style="background-color: rgba(0, 0, 0, 0.1) " >Score</th>';
 
-    // const btnGame = document.createElement("button")
-    // btnGame.innerText = "Retournez au jeu"
-
-    // main.appendChild(btnGame)
-    const btnGame = document.createElement("img");
-
-    btnGame.src = homepageIcone;
-    btnGame.style.width = '240px';  
-    btnGame.style.height = '180px'; 
-    btnGame.style.marginTop = "24%";
-
-    main.appendChild(btnGame)
-
-    btnGame.addEventListener("mouseover", () => {
-        btnGame.src = homepageIconeHover;
+    table.appendChild(header);
+    scores.forEach(score => {
+        const row = document.createElement('tr');
+        row.innerHTML = `<th style="border-right: rgba(255, 255, 255, 0.5) dashed 1px; border-top: rgba(255, 255, 255, 0.5) solid 1px; padding-bottom: 20px; padding-top: 20px">${score.username}</td>
+                         <td style="border-top: rgba(255, 255, 255, 0.5) solid 1px; padding-bottom: 20px; padding-top:20px ;">${score.score}</td>`;
+        table.appendChild(row);
     });
-    
-    btnGame.addEventListener("mouseout", () => {
-        btnGame.src = homepageIcone;
-    });
-    
 
-    btnGame.addEventListener("click", (event => {
-        event.preventDefault()
-        Navigate('/')
-    }))
+    table.style.backgroundColor = 'rgba(230, 230, 230, 0.5)';
+    table.style.border = '5px solid white';
+    table.style.borderRadius = '10px';
+    table.style.borderCollapse = 'inherit';
+    table.style.margin = 'auto';
+    table.style.width = '60%';
+    table.style.textAlign = 'center';
+    table.style.fontSize = '1.5rem';
+    table.style.tableLayout = 'fixed';
+    table.style.marginBottom = '50px';
+    main.appendChild(table);
 };
-  
-  export default LeaderboardPage;
-  
+
+export default LeaderboardPage;
