390d45135a5815682f900b2ffa82511a39ad2d8a
Ysaline-Degols-Vinci <128386131+Ysaline-Degols-Vinci@users.noreply.github.com>
2023-12-17 14:05:39 +0100

    cleaning code

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,11 +1,9 @@
 import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
-import NewPage from '../Pages/NewPage';
 import LoginPage from '../Pages/loginPage';
 import RegisterPage from '../Pages/RegisterPage';
 import LeaderboardPage from '../Pages/LeaderboardPage';
 import TutorielPage from '../Pages/TutorielPage';
-import MenuPage from '../Pages/MenuPage'
 import MenuCat from '../Pages/MenuCat';
 import CoffeeMenuPage from '../Pages/MenuCoffee'
 
@@ -13,12 +11,10 @@ import CoffeeMenuPage from '../Pages/MenuCoffee'
 const routes = {
   '/': HomePage,
   '/game': GamePage,
-  '/new': NewPage,
   '/login': LoginPage,
   '/register': RegisterPage,
   '/leaderboard': LeaderboardPage,
   '/tutoriel': TutorielPage,
-  '/menu' : MenuPage,
   '/menucoffee' : CoffeeMenuPage,
   '/menucat': MenuCat
 };
